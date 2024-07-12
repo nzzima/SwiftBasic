728423cfaeb489d57d5bc1224489b4949bc003b7
nzzima <nikita-krylov-2012@mail.ru>
2024-07-12 11:26:29 +0300

    Comment lines in main for sem1

--- main.swift
+++ main.swift
@@ -7,14 +7,16 @@
 
 import Foundation
 
-print("Result of task1:")
-printEven()
-
-print("Result of task2: ")
-printMoreLess(number1: 7, number2: 20)
-
-print("Result of task3: ")
-plusK_IfElse()
-
-print("Result of task4: ")
-plusK_break_IfElse()
+//--------------Seminar1 (StartingSwift folder)--------------
+//print("Result of task1:")
+//printEven()
+//
+//print("Result of task2: ")
+//printMoreLess(number1: 7, number2: 20)
+//
+//print("Result of task3: ")
+//plusK_IfElse()
+//
+//print("Result of task4: ")
+//plusK_break_IfElse()
+//--------------Seminar1 (StartingSwift folder)--------------
