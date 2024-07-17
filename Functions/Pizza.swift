b33c6e5d6e8a2306f82fc571fe9ab0d3c6933636
nzzima <nikita-krylov-2012@mail.ru>
2024-07-17 15:40:45 +0300

    Added pizza task

--- Functions/Pizza.swift
+++ Functions/Pizza.swift
@@ -7,8 +7,34 @@
 
 import Foundation
 
-enum Pizza {
-    case thin
-    case classic
-    case thick
+enum Pizza: String{
+    case Margarita
+    case FourSeason
+    case Peperonni
 }
+
+func enumerationPizza() {
+    print("Enter your favorite pizza number:\n1 - Margarita\n2- FourSeason\n3 - Peperonni")
+    let chooseString = readLine() ?? ""
+    let chooseInt = Int(chooseString)
+    
+    let margarita = Pizza.Margarita
+    let fourSeason = Pizza.FourSeason
+    let peperonni = Pizza.Peperonni
+    
+    if (chooseInt == 1) {
+        print(margarita.rawValue)
+    }
+    else if (chooseInt == 2) {
+        print(fourSeason.rawValue)
+    }
+    else if (chooseInt == 3)  {
+        print(peperonni.rawValue)
+    }
+    else {
+        print("Please write correct number of pizze!")
+    }
+}
+
+
+
