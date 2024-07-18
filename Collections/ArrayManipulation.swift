f8e67b085ae34f4ac7ede033d0810ab315e43a5b
nzzima <nikita-krylov-2012@mail.ru>
2024-07-18 14:26:37 +0300

    U

--- Collections/ArrayManipulation.swift
+++ Collections/ArrayManipulation.swift
@@ -7,21 +7,12 @@
 
 import Foundation
 
-let oldArray = ["-4", "5", "10", nil, "4", nil, "25", "0", nil, "16", "75", "-20", "-7", "15", "0", nil]
-let block = [nil, "0", "4"]
+let oldArray = [-4, 5, 10, nil, 4, nil, 25, 0, nil, 16, 75, -20, -7, 15, 0, nil]
 
 func arrayManipulation() {
-    var newArray: [String] = []
-    for element in oldArray {
-        for blockElem in block {
-            if (element != blockElem) {
-                newArray.append(element!)
-                break
-            }
-            else {
-                continue
-            }
-        }
-    }
-    print("Old array -> \(oldArray)\nNew aaray -> \(newArray)")
+    var newArray: [Int] = []
+    newArray = oldArray.compactMap{ $0 } //Delete 'nil' elements in 'oldArray'
+    newArray = newArray.filter{ $0 != 0 }.filter{ $0 != 4 } //Delete '0' and '4' from 'newArray'
+    newArray.sort()
+    print(newArray)
 }
