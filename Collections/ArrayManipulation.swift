32c19b4689a0fde7211116cd95512a11a56583f3
nzzima <nikita-krylov-2012@mail.ru>
2024-07-18 16:31:50 +0300

    Modified array manipulation task

--- Collections/ArrayManipulation.swift
+++ Collections/ArrayManipulation.swift
@@ -10,9 +10,6 @@ import Foundation
 let oldArray = [-4, 5, 10, nil, 4, nil, 25, 0, nil, 16, 75, -20, -7, 15, 0, nil]
 
 func arrayManipulation() {
-    var newArray: [Int] = []
-    newArray = oldArray.compactMap{ $0 } //Delete 'nil' elements in 'oldArray'
-    newArray = newArray.filter{ $0 != 0 }.filter{ $0 != 4 } //Delete '0' and '4' from 'newArray'
-    newArray.sort()
+    let newArray = oldArray.compactMap{ $0 }.filter{$0 != 0 && $0 != 4}.sorted{$0 < $1}
     print(newArray)
 }
