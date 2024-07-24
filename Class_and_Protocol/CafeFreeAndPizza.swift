37668b71697345ea5cc7d4ca6e86c2848e818bf5
nzzima <nikita-krylov-2012@mail.ru>
2024-07-24 11:30:03 +0300

    FInished cafeterium task (class/protocols/extension)

--- Class_and_Protocol/CafeFreeAndPizza.swift
+++ Class_and_Protocol/CafeFreeAndPizza.swift
@@ -6,3 +6,101 @@
 //
 
 import Foundation
+
+protocol StructMenuProtocol {
+    var name: String {get}
+    var cost: Double {get}
+}
+
+protocol CafeteriumStatus {
+    func open()
+    func closed()
+}
+
+struct NewPizza: StructMenuProtocol {
+    enum PizzaType {
+        case margarita
+        case pepperoni
+        case vegan
+        case meal
+        case spicy
+    }
+    
+    enum DoughType {
+        case thin
+        case thick
+    }
+    
+    enum Ingredient {
+        case pepperoni
+        case tomatoes
+        case cheese
+        case jalapeño
+    }
+    var cost: Double
+    var pizzaType: PizzaType
+    var doughtType: DoughType
+    var ingredient: Ingredient
+    var name: String
+}
+
+struct FrenchFries: StructMenuProtocol {
+    enum Size {
+        case large
+        case small
+        case medium
+    }
+    
+    var cost: Double
+    var size: Size
+    var name: String
+}
+
+class Cafeterium {
+    private var menu: [StructMenuProtocol]
+    var status: Bool
+    
+    init(menu: [StructMenuProtocol], status: Bool) {
+        self.menu = menu
+        self.status = status
+    }
+    
+    func getMenu() -> [StructMenuProtocol] {
+        return menu
+    }
+    
+    func getStatus() -> Bool {
+        return status
+    }
+    
+    func addPizza(pizza: NewPizza) {
+        if getStatus() {
+            menu.append(pizza)
+        }
+        else {
+            print("Can't add new position to menu, cafeterium is closed")
+        }
+    }
+    
+    func addFrenchFries(frenFries: FrenchFries) {
+        if getStatus() {
+            menu.append(frenFries)
+        }
+        else {
+            print("Can't add new position to menu, cafeterium is closed")
+        }
+    }
+}
+
+extension Cafeterium: CafeteriumStatus {
+    
+    func open() {
+        print("Cafeterium is open!")
+        status = true
+    }
+    
+    func closed() {
+        print("Cafeterium is closed!")
+        status = false
+    }
+}
--- main.swift
+++ main.swift
@@ -20,7 +20,7 @@ import Foundation
 //print("Result of task4: ")
 //plusKBreakIfElse()
 //--------------Seminar1 (StartingSwift folder)--------------
-//-----------------------------------------------------------
+
 //----------------Seminar2 (Functions folder)----------------
 //let profit = profitOnBankDeposit(sum: 7000.0, percent: 18.0, years: 2)
 //print("Total profit -> \(profit)")
@@ -28,7 +28,7 @@ import Foundation
 //print("Result of pizza: ")
 //enumerationPizza()
 //----------------Seminar2 (Functions folder)----------------
-//-----------------------------------------------------------
+
 //----------------Seminar3 (Collections folder)--------------
 //print("Result of buying tea: ")
 //printTeaHumans()
@@ -37,17 +37,37 @@ import Foundation
 //var array = createArray(length: 8)
 //print("Result array -> \(array)")
 //----------------Seminar3 (Collections folder)--------------
-//--------------Seminar4 (Class/structure folder)------------
-var array: [pizza] = []
-var cafe = Cafe(pizzas: array)
-
-cafe.addPizza(newPizza: pizza(cost: 100, pizzaType: .margarita, doughtType: .thick, ingredient: .jalapeño))
-cafe.addPizza(newPizza: pizza(cost:200, pizzaType: .pepperoni, doughtType: .thin, ingredient: .pepperoni))
-cafe.addPizza(newPizza: pizza(cost:150, pizzaType: .meal, doughtType: .thick, ingredient: .tomatoes))
-cafe.addPizza(newPizza: pizza(cost: 125, pizzaType: .spicy, doughtType: .thick, ingredient: .jalapeño))
-
-cafe.printAll()
-let allPizzas = cafe.getAll()
-
 
 //--------------Seminar4 (Class/structure folder)------------
+//var array: [pizza] = []
+//var cafe = Cafe(pizzas: array)
+//
+//cafe.addPizza(newPizza: pizza(cost: 100, pizzaType: .margarita, doughtType: .thick, ingredient: .jalapeño))
+//cafe.addPizza(newPizza: pizza(cost:200, pizzaType: .pepperoni, doughtType: .thin, ingredient: .pepperoni))
+//cafe.addPizza(newPizza: pizza(cost:150, pizzaType: .meal, doughtType: .thick, ingredient: .tomatoes))
+//cafe.addPizza(newPizza: pizza(cost: 125, pizzaType: .spicy, doughtType: .thick, ingredient: .jalapeño))
+//
+//let allPizzas = cafe.getAll()
+//cafe.printAll()
+//--------------Seminar4 (Class/structure folder)------------
+
+
+//---------Seminar5 (Class/protocols/extensions folder)-------
+var menu: [StructMenuProtocol] = []
+var cafeStatus = false
+var cafeterium = Cafeterium(menu: menu, status: cafeStatus)
+
+cafeterium.addPizza(pizza: NewPizza(cost: 80, pizzaType: .pepperoni, doughtType: .thick, ingredient: .cheese, name: "Season pizza!"))
+//Check terminal% can't add this position!
+
+cafeterium.open()
+//Cafeterium open, now you can add positions!
+
+cafeterium.addPizza(pizza: NewPizza(cost: 80, pizzaType: .pepperoni, doughtType: .thick, ingredient: .cheese, name: "Season pizza!"))
+cafeterium.addFrenchFries(frenFries: FrenchFries(cost: 40, size: .large, name: "Spicy summer french fries!"))
+cafeterium.addPizza(pizza: NewPizza(cost: 120, pizzaType: .meal, doughtType: .thin, ingredient: .jalapeño, name: "Season spicy pizza!"))
+cafeterium.addFrenchFries(frenFries: FrenchFries(cost: 25, size: .small, name: "Classic french fries!"))
+
+let dataMenu = cafeterium.getMenu()
+print(dataMenu)
+//---------Seminar5 (Class/protocols/extensions folder)-------
